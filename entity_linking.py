f858d47650e60ff14e78800ee178e9416e1ad157
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2022-01-06 07:39:56 +0000

    delete README.md

--- entity_linking.py
+++ entity_linking.py
@@ -3,4 +3,4 @@
 import sys
 from wikipedia2vec.dictionary import Dictionary
 from wikipedia2vec.mention_db import MentionDB
-from wikipedia2vec.utils.tokenizer.mecab_toke
+from wikipedia2vec.utils.tokenizer.mecab_token

7d3480c89e7b60b2319061503910a9f87410d010
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2022-04-09 11:21:35 +0000

    adding badge

--- entity_linking.py
+++ entity_linking.py
@@ -5,4 +5,4 @@ from wikipedia2vec.dictionary import Dictionary
 from wikipedia2vec.mention_db import MentionDB
 from wikipedia2vec.utils.tokenizer.mecab_tokenizer import MeCabTokenizer
 
-dic = Diction
+dic = Dictiona

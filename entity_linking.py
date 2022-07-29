caa739d658a663c05c732776d7e505441978867c
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2022-07-29 08:31:44 +0000

    Update Liberland-constitution.md

--- entity_linking.py
+++ entity_linking.py
@@ -6,4 +6,4 @@ from wikipedia2vec.mention_db import MentionDB
 from wikipedia2vec.utils.tokenizer.mecab_tokenizer import MeCabTokenizer
 
 dic = Dictionary.load(sys.argv[2])
-db = MentionDB.load(sys.argv[3], di
+db = MentionDB.load(sys.argv[3], dic

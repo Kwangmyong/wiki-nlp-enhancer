20aaa8f0aa9314446cf6a96a0d431dff2b7da259
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2022-08-20 16:04:33 +0000

    3rd edit by Lara

--- entity_linking.py
+++ entity_linking.py
@@ -8,4 +8,4 @@ from wikipedia2vec.utils.tokenizer.mecab_tokenizer import MeCabTokenizer
 dic = Dictionary.load(sys.argv[2])
 db = MentionDB.load(sys.argv[3], dic)
 with open(sys.argv[1]) as f:
-  te
+  tex

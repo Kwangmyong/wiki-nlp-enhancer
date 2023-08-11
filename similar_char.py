0410b25d6b74e7ef176e42ac1c04c8611af675ec
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2023-08-11 13:55:54 +0000

    Initial WhiteSource configuration file

--- similar_char.py
+++ similar_char.py
@@ -11,3 +11,5 @@ pair_counter = Counter()
 
 for (title1, title2) in dump_db.redirects():
     ops = Levenshtein.editops(title1.lower(), title2.lower())
+    if len(ops) == 1:
+        (op, p1, p2) = ops[0]

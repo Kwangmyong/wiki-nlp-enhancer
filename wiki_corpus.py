95134a677276795503dddc91d88cd0b87ec04b34
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2023-09-26 17:19:21 +0000

    cal

--- wiki_corpus.py
+++ wiki_corpus.py
@@ -21,3 +21,8 @@ def parse_lines(lines):
   g = Graph()
   g.parse(data=u'\n'.join(lines), format='n3')
   return g
+
+with bz2.BZ2File(sys.argv[1]) as in_file:
+  for (_, p, o) in read_ttl(in_file):
+    if p.toPython() == 'http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString':
+      print(o.toPython())

2fa21e6f203371a5eb1d8572db72d7efb163e3cb
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2023-10-27 11:42:12 +0000

    first version

--- word_dic.py
+++ word_dic.py
@@ -4,3 +4,6 @@
 import sys
 from wikipedia2vec.dictionary import Dictionary
 from wikipedia2vec.mention_db import MentionDB
+
+dic = Dictionary.load(sys.argv[1])
+db = MentionDB.load(sys.argv[2], dic)

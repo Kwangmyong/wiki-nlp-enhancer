b63d55f8b440a718dcf71362d5d636c557f8c4c8
Kwangmyong <112688582+Kwangmyong@users.noreply.github.com>
2023-10-30 11:30:31 +0000

    fixed a spelling error

--- word_dic.py
+++ word_dic.py
@@ -8,3 +8,7 @@ from wikipedia2vec.mention_db import MentionDB
 dic = Dictionary.load(sys.argv[1])
 db = MentionDB.load(sys.argv[2], dic)
 words = set()
+
+for mention in db:
+  if mention.link_prob >= 0.1:
+    if mention.text not in words:
